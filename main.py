7291c6be3d2d46d174d4fb684c6661a49a8754e8
Alexander Reben <artBoffin@users.noreply.github.com>
2017-11-20 15:19:36 -0800

    some test text and QR

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
+#!/usr/bin/python
+# -*- coding: UTF8 -*-
 from escpos.printer import Usb
 
-""" Seiko Epson Corp. Receipt Printer (EPSON TM-T88III) """
 p = Usb(0x04b8,0x0e15)
-p.text("Hello World\n")
-#p.barcode('1324354657687', 'EAN13', 64, 2, '', '')
+p.set(font='a', height=1, align='center')
+p.text("Font a\n")
+p.set(font='b', height=1, align='center')
+p.text("Font b\n")
+p.set(font='a', height=1, align='center')
+p.text("--------------------------------\n")
+p.set(font='a', height=2, align='center')
+p.text("Font a height 2\n")
+p.set(font='b', height=2, align='center')
+p.text("Font b height 2\n")
+p.qr("This QR code is readable by phone")
 p.cut()
